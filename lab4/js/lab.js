a21c239d53eb0f576b4fe87dfaa08931f61219d0
AlexanderLA17 <80607202+AlexanderLA17@users.noreply.github.com>
2025-04-23 21:39:43 -0700

    Lab 5 (Solo)
    Moving everything to Lab 5.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Hangman
+// Author: Alexander Adams
+// Date: 4/21/2025
 
 // Constants
+//Hangman:
+
+//Create a counter that will make you lose the game when 6 guesses have failed.
+
+//Create a word limit which should be longer or shorter depending on how difficult you want the game to be.
+
+//Create a random word generator API using something like Supabase or manually add words yourself.
+
+//Accept user input. (Restrict to letter guesses only)
+
+//Evaluate possible moves (can’t guess same letter twice)
+
+//Restrict letters not used in word or phrase.
+
+//Play with a friend and try to guess the phrase before them!
 
 // Functions
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+* Author: Alexander Adams (alleadam@ucsc.edu)
+* Created: 4 / 23 / 25
+* License: Public Domain
+*/
 
-// Constants
+// Define Variables
 
-// Functions
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// calculate
+age = 2019 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
-// let's get this party started
-main();

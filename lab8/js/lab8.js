3e224ae11e11d96559bc876574cf43664ff0a895
AlexanderLA17 <80607202+AlexanderLA17@users.noreply.github.com>
2025-05-08 22:19:20 -0700

    Lab 8 - More fixes
    Lab 8 - More fixes

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -8,16 +8,13 @@ function isEven(x){
 console.log("Is 1 even? ", isEven(1));
 console.log("Is 2 even? ", isEven(2));
 
-array = [100, 81, 4, 16, 42, 144, 10000]
+let array = [100, 81, 4, 16, 42, 144, 10000];
 console.log("My array", array);
 
-var result = array.map(isEven);
-
+let result = array.map(isEven);
 console.log("Test of evenness of array", result);
 
-var result = array.map(function(x){
+result = array.map(function(x){
   return x ** 0.5;
-})
-
+});
 console.log("Squareroot of array:", result);
-

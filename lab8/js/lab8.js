6a3ddc254177bc5e1b08595657c2989818d3bba4
AlexanderLA17 <80607202+AlexanderLA17@users.noreply.github.com>
2025-05-16 13:13:19 -0700

    Lab 10 update + lab 9 incomplete stuff
    Basically just all requirements for lab 10 + some WIP stuff for lab 9.

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -18,3 +18,9 @@ result = array.map(function(x){
   return x ** 0.5;
 });
 console.log("Squareroot of array:", result);
+
+// Your map results data
+var mapResults = "Your map results here";
+
+// Use jQuery to select the element by its ID and set the HTML content
+$("#output").html(mapResults);

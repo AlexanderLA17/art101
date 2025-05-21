72491b34db83b7b265015f64a39e7ac6bc47b6dd
AlexanderLA17 <80607202+AlexanderLA17@users.noreply.github.com>
2025-05-20 18:08:54 -0700

    random crap changing
    asdfjlkfsdahjklfdshjkdfs

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -8,12 +8,12 @@
 */
 
 $(document).ready(function() {
-  // Sorts the characters of a string in alphabetical order.
+
   function sortString(inputString) {
     return inputString.split('').sort().join('');
   }
 
-  // Generates random text from Lorem Ipsum
+ 
   function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
@@ -23,14 +23,14 @@ $(document).ready(function() {
     return text.slice(randStart, randStart + randLen);
   }
 
-  // Listener for name sorting
+
   $("#submit").click(function() {
     const userName = $("#user-name").val();
     const userNameSorted = sortString(userName);
     $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
   });
 
-  // Listener for fake dialogue
+
   $("#make-convo").click(function() {
     const newText = generateRandomText();
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
